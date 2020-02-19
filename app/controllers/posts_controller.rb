59c3e2c0c68aed67211dd9c4e403600bbd21511a
Varun Reddy Regalla <varunreddy1061@gmail.com>
2020-02-19 13:17:50 -0500

    filtered activity feed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update, :activity_feed, :index]
   def index
-    @posts = Post.order(created_at: :desc).paginate(page: params[:page])
+    @posts = Post.order(created_at: :desc).paginate(page: params[:page]).where.not(user_id: current_user)
   end
 
   def activity_feed
     
-    @posts = Post.order(created_at: :desc).paginate(page: params[:page]).where(user_id: [current_user, current_user.following.pluck(:id)])
+    @posts = Post.where(user_id: [current_user.following.pluck(:id).flatten << current_user.id]).order(created_at: :desc).paginate(page: params[:page])
     render 'activity_feed'
   end
 
