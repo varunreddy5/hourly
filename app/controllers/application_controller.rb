b6b1645db97429feec3f0f09b71f6c146270e2eb
Varun Reddy Regalla <varunreddy1061@gmail.com>
2020-03-09 22:31:36 -0400

    added multiple oauth providers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # protect_from_forgery with: :exception
-  # include ActionController::Live
-  
-  # protect_from_forgery prepend: true
   include Pagy::Backend
-  # def logged_in_user
-  #   unless logged_in?
-  #     store_location
-  #     flash[:danger] = "Please Login"
-  #     redirect_to login_path
-  #   end
-  # end
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username])
-    # devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password, :remember_me])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username])
   end
 end
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,30 +1,75 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  before_action :set_service
+  before_action :set_user
+  attr_reader :service, :user
   def facebook
     # Next we need to lookup the user if the record is present in the database else we need to create a new user
-    service = Service.where(provider: auth.provider, uid: auth.uid).first
-    if service.present?
-      user = service.user
-    else
-      user = User.new(
-        email: auth.info.email,
-        name: auth.info.name,
-        username: auth.info.email.split('@')[0],
-        password: Devise.friendly_token[0, 20]
-      )
-      if user.save
-        user.services.create(
-          provider: auth.provider,
-          uid: auth.uid,
-          expires_at: Time.at(auth.credentials.expires_at),
-          access_token: auth.credentials.token
-        )
-      end
-    end
-    sign_in_and_redirect user, event: :authentication
-    set_flash_message :notice, :success, kind: "Facebook"
+    handle_auth("Facebook")
   end
 
+  def github
+    handle_auth("Github")
+  end
+
+  def twitter
+    handle_auth("Twitter")
+  end
+
+  private 
+  def handle_auth(kind)
+    if service.present?
+      service.update(service_attr)
+    else
+      user.services.create(service_attr)
+    end
+
+    if user_signed_in?
+      flash[:notice] = "Your #{kind} account was connected"
+      redirect_to edit_user_registration_path
+    else
+      sign_in_and_redirect user, event: :authentication
+      set_flash_message :notice, :success, kind: kind
+    end
+  end
   def auth
     request.env['omniauth.auth']
   end
+  def set_service
+    @service = Service.where(provider: auth.provider, uid: auth.uid).first
+  end
+
+  # This validates if user already have an account associated with either facebook or github
+  def set_user
+    if user_signed_in?  # If user is signed in
+      @user = current_user
+    elsif service.present? # If user is not signed in and service is present
+      @user = service.user
+    elsif User.where(email: auth.info.email).any? # If the user is not signed in and an account already exists
+        flash[:alert] = "An account with this email already exists. Please signin with that account before connecting your #{auth.provider} account"
+        redirect_to new_user_session_path
+    else
+      username = auth.info.nickname.present? ? auth.info.nickname : auth.info.email.split('@')[0]
+      @user = User.new(     # Create a new user with auth credentials
+        email: auth.info.email, 
+        name: auth.info.name,
+        username: username,
+        password: Devise.friendly_token[0, 20]
+      )
+      if !@user.save
+        flash[:alert] = "Your email or username of your #{auth.provider} account is not accessible. Please choose a different login method"
+        redirect_to new_user_session_path
+      end
+    end
+  end
+  def service_attr
+    expires_at = auth.credentials.expires_at.present? ? Time.at(auth.credentials.expires_at) : nil
+    {
+      provider: auth.provider,
+      uid: auth.uid,
+      expires_at: expires_at,
+      access_token: auth.credentials.token,
+      access_token_secret: auth.credentials.secret,
+    }
+  end
+  
 end
