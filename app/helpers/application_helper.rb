fdcc2c5bc392d87a2c16f0a44ce9119f5f29e7d4
Varun Reddy Regalla <varunreddy1061@gmail.com>
2020-04-02 22:50:07 -0400

    refactoring ui

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
   # include SessionsHelper
   include Pagy::Frontend
 
-  def user_avatar(user, size=30)
+  def user_avatar(user, size=40)
     if user.avatar.attached?
      image_tag user.avatar.variant(resize: "#{size}x#{size}!"), class: "rounded-circle"
     else
-      image_tag "user.svg", size: "#{size}x#{size}", class: "rounded-circle"
+      image_tag "user.jpg", size: "#{size}x#{size}", class: "rounded-circle user-avatar"
     end
   end
 end
