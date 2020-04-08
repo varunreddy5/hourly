6eed86093f1b85ed3f52141539e2d108bc8e0386
Varun Reddy Regalla <varunreddy1061@gmail.com>
2020-04-07 23:27:39 -0400

    sessions controller refactored

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,3 @@
 class SessionsController < ApplicationController
-  # def new
-  #   if logged_in?
-  #     redirect_to current_user
-  #   end
-  # end
-  # def create
-  #   user = User.find_by(email: params[:session][:email].downcase)
-  #   if user && !!user.authenticate(params[:session][:password])
-  #     log_in user
-  #     remember user
-  #     redirect_to user
-  #   else
-  #     flash[:danger] = 'Invalid email/password. Try Again!'
-  #     render 'new'
-  #   end
-  # end
-  # def destroy
-  #   log_out if logged_in?
-  #   redirect_to root_path
-  # end
+  
 end
