2d2810255996e65b8c0d6efafdc32aa79cca7e4b
Varun Reddy Regalla <varunreddy1061@gmail.com>
2020-04-28 02:23:59 -0400

    removed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,29 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name: "foobar", email: "foobar@example.com", username: 'foobar',current_position: "Software Developer at Google", password: "varun2@7", password_confirmation: "varun2@7")
+# User.create!(name: "foobar", email: "foobar@example.com", username: 'foobar',current_position: "Software Developer at Google", password: "varun2@7", password_confirmation: "varun2@7")
 
-companies = ["Google", "Facebook", "Amazon", "GitHub", "Apple"]
-49.times do |n|
-  name = Faker::Name.name
-  email = "user-#{n+1}@example.com"
-  password = "password"
-  User.create!(name: name, email: email, username:"user-#{n+1}", current_position: "Software Developer at #{companies[n%companies.count]}" ,password: password, password_confirmation: password)
-end
+# companies = ["Google", "Facebook", "Amazon", "GitHub", "Apple"]
+# 49.times do |n|
+#   name = Faker::Name.name
+#   email = "user-#{n+1}@example.com"
+#   password = "password"
+#   User.create!(name: name, email: email, username:"user-#{n+1}", current_position: "Software Developer at #{companies[n%companies.count]}" ,password: password, password_confirmation: password)
+# end
 
 
-users = User.all
-50.times do |p|
-  content = Faker::Lorem.sentence(word_count: 30)
-  users.each{ |user| user.posts.create!(content: content)}
-end
+# users = User.all
+# 50.times do |p|
+#   content = Faker::Lorem.sentence(word_count: 30)
+#   users.each{ |user| user.posts.create!(content: content)}
+# end
 
-users = User.all
-user  = users.first
-following = users[20..30]
-# followers = users[40..50]
-following.each { |followed| user.follow(followed) }
-# followers.each { |follower| follower.follow(user) }
+# users = User.all
+# user  = users.first
+# following = users[20..30]
+# # followers = users[40..50]
+# following.each { |followed| user.follow(followed) }
+# # followers.each { |follower| follower.follow(user) }
 
 
 
