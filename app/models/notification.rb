6a6168a6280159b991d323f622bf0659236d1414
Varun Reddy Regalla <varunreddy1061@gmail.com>
2020-04-28 01:53:00 -0400

    notifications recipient

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,6 @@
 class Notification < ApplicationRecord
   belongs_to :user
-  belongs_to :recipient, class_name: "User"
+  belongs_to :recipient, class_name: "User", dependent: :destroy
   belongs_to :notifiable, polymorphic: true
 
   scope :unread, ->{ where(read_at: nil) }
