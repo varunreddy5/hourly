9c1c2028f99675c881b5f09e5c4128853e7986a1
Varun Reddy Regalla <varunreddy1061@gmail.com>
2020-05-07 09:46:04 -0400

    created docker-stack and dockerfile for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.hosts << "192.168.99.100" # added by Varun
+  config.action_cable.allowed_request_origins = ["http://192.168.99.100"]
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
